9f169d239c35736a6ed426cd33216fa2eedd33ca
gusam <gusam@DESKTOP-GEF5I83>
2022-12-25 11:15:03 +0900

    두번째 커밋

--- SpringMVC01/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/board/controller/BoardController.java
@@ -1,5 +1,35 @@
 package kr.board.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.board.entity.Board;
+
+@Controller //브라우저에서 컨트롤러 부터 시작한다.
 public class BoardController {
 
+	// boardList.do라는 핸들의 왔을때 처리하는 것을 핸들러매핑이라고 한다.
+	// HandlerMapping
+	@RequestMapping("/boardList.do")
+	public String boardList(Model model) {
+		Board vo = new Board();
+		vo.setIdx(1);
+		vo.setTitle("게시판실습");
+		vo.setContent("게시판실습");
+		vo.setWriter("김태경");
+		vo.setIndate("2022-05-10");
+		vo.setCount(0);
+		
+		List<Board> list = new ArrayList<Board>();
+		list.add(vo);
+		list.add(vo);
+		list.add(vo);
+		model.addAttribute("list", list);
+		return "boardList";// /WEB-INF/views/boardList.jsp -> forward
+	}
+	
 }
--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import kr.board.entity.Board;
 
-//@Mapper - Mybatis API
+//@Mapper - Mybatis API가 가지고 있는 옵션이라 추가를 해줘야 사용 가능하다.
 //매퍼인터페이스와 매퍼파일의 이름이 기본적으로 같아야 한다.
 //configuratin file 마이바티스할때 필요한데 스프링에서는 필요없다.
 public interface BoardMapper {
-	public List<Board> getLists();
+	public List<Board> getLists(); //전체리스트 가져오는 메서드
+
+
+
 }
